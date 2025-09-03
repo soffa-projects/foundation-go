bf4b6548a2ba9c1792c7b95cfe51abdafc5eeda3
Titus <titus@gahissy.com>
2025-09-03 12:07:13 +0100

    chore: improvements

---
+++ h/config.go
@@ -0,0 +1,23 @@
+package h
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+	log "github.com/sirupsen/logrus"
+)
+
+func LoadEnv(cfg any) {
+	env := os.Getenv("ENV")
+	if env != "production" && env != "prod" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Warnf("unable to load .env file: %v", err)
+		}
+	}
+	err := envconfig.Process("", cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
